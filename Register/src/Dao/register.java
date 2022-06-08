d35c67cae713e0f20052fc850a08c98f88a87dc5
Mrgeniv <jorve2@schule.bremen.de>
2022-06-08 11:30:31 +0200

    Objekte

---
+++ Register/src/Dao/register.java
@@ -0,0 +1,14 @@
+package Dao;
+
+public class register {
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String password;
+    private String passwordSave;
+    private String registerDate;
+
+
+
+}
